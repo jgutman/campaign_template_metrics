4e7d09d7ca292b9f99c89cd24db2fe95012b2e88
Jacqueline Gutman <jacqueline.gutman@gmail.com>
2017-12-14 08:54:01 -0500

    set up with functional query generation script to prepare for complete refactoring

---
+++ Dest Dinner New Customer Offer/generated_query.sql
@@ -0,0 +1,103 @@
+
+    WITH campaign_lists AS (
+        SELECT a.*,
+          md.internal_user_id IS NOT NULL AS responder,
+          pp.promo_period
+        FROM analytics.dest_dinner_new_customer a
+        LEFT JOIN dw.marketing_discounts md
+          ON md.internal_user_id = a.user_id
+          AND ((md.discount_name = '$5 off next 10 boxes'
+        AND a.test_group IN ('test'))
+        )
+        CROSS JOIN (
+        SELECT 'promo_period' AS promo_period
+        ) pp
+    ),
+      last_subscription_change AS (
+        SELECT a.user_id,
+          CASE WHEN DATE(convert_timezone('America/New_York',
+            subscription_changed_at)) <= '2017-12-12'
+    THEN 'promo_period'
+     END AS promo_period,
+          max(subscription_changed_at) AS last_change
+        FROM campaign_lists a
+        INNER JOIN dw.user_subscription_events use
+          ON a.user_id = use.internal_user_id
+          AND DATE(convert_timezone('America/New_York',
+            subscription_changed_at)) >= '2017-10-02'
+        GROUP BY 1,2
+    ),
+      cum_subscription_changes AS (
+      		SELECT lsc.user_id,
+        text('promo_period') AS promo_period,
+        max(last_change) AS last_change
+        FROM last_subscription_change lsc
+        WHERE promo_period IN ('promo_period')
+        GROUP BY 1
+    
+    ),
+      subscription_changes AS (
+        SELECT use.internal_user_id,
+          csc.promo_period,
+          use.subscription_status_change_event
+        FROM cum_subscription_changes csc
+        INNER JOIN dw.user_subscription_events use
+          ON csc.user_id = use.internal_user_id
+          AND csc.last_change = use.subscription_changed_at
+    ),
+      boxes_ordered AS (
+        SELECT bo.internal_user_id,
+          CASE WHEN ship_date <= '2017-12-12'
+    THEN 'promo_period'
+     END AS promo_period,
+          COUNT(*) AS total_boxes_ordered,
+          SUM(CASE WHEN nth_delivery = 1 THEN 1 END) AS _1st_boxes_ordered,
+          SUM(CASE WHEN nth_delivery = 4 THEN 1 END) AS _4th_boxes_ordered,
+          SUM(CASE WHEN nth_delivery = 6 THEN 1 END) AS _6th_boxes_ordered,
+          SUM(gov) AS gov,
+          SUM(CASE WHEN dessert_plates > 0 THEN 1 END) AS desserts_ordered
+      FROM campaign_lists a
+      INNER JOIN dw.menu_order_boxes bo
+      ON bo.internal_user_id = a.user_id
+        AND ship_date >= '2017-10-02'
+        AND status <> 'canceled'
+      GROUP BY 1,2
+    ),
+      individual_metrics AS (
+        SELECT a.*,
+          COALESCE(sc.subscription_status_change_event = 'cancelation',
+            false) AS canceled,
+          COALESCE(sc.subscription_status_change_event = 'activation',
+            false) AS activated,
+          COALESCE(sc.subscription_status_change_event = 'reactivation',
+            false) AS reactivated,
+          COALESCE(bo.total_boxes_ordered, 0) AS total_boxes_ordered,
+	COALESCE(bo.gov, 0) AS gov,
+	COALESCE(bo._1st_boxes_ordered, 0) AS _1st_boxes_ordered,
+	COALESCE(bo._4th_boxes_ordered, 0) AS _4th_boxes_ordered,
+	COALESCE(bo._6th_boxes_ordered, 0) AS _6th_boxes_ordered,
+	COALESCE(bo.desserts_ordered, 0) AS desserts_ordered
+      FROM campaign_lists a
+      LEFT JOIN subscription_changes sc
+      ON sc.internal_user_id = a.user_id
+        AND sc.promo_period = a.promo_period
+      LEFT JOIN boxes_ordered bo
+      ON bo.internal_user_id = a.user_id
+        AND bo.promo_period = a.promo_period
+    )
+      SELECT a.test_group,
+		a.responder,
+        promo_period,
+        COUNT(*) AS total,
+        SUM(CASE WHEN canceled THEN 1 END) AS cancelations,
+        SUM(CASE WHEN activated THEN 1 END) AS new_activations,
+        SUM(CASE WHEN reactivated THEN 1 END) AS reactivations,
+        SUM(total_boxes_ordered) AS total_boxes_ordered,
+	SUM(gov) AS gov,
+	SUM(_1st_boxes_ordered) AS _1st_boxes_ordered,
+	SUM(_4th_boxes_ordered) AS _4th_boxes_ordered,
+	SUM(_6th_boxes_ordered) AS _6th_boxes_ordered,
+	SUM(desserts_ordered) AS desserts_ordered
+      FROM individual_metrics a
+      GROUP BY 1, 2, 3
+    
